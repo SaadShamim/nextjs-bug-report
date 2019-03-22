171829d475f9eda7df7de5d0ba782b6bc4460ffe
Saad <saadshamim@gmail.com>
2019-03-22 13:13:49 -0400

    remove nextjs

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+//const handle = app.getRequestHandler()
 
 const routeHandler = require('./handler');
 
-app.prepare().then(() => {
+// app.prepare().then(() => {
   const server = express()
 
   server.get('/a', (req, res) => {
@@ -25,12 +25,12 @@ app.prepare().then(() => {
 
   server.get('/derp', routeHandler.derp);
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+  // server.get('*', (req, res) => {
+  //   return handle(req, res)
+  // })
 
   server.listen(port, err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
-})
+// })
